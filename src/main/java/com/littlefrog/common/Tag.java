46ee2a8393e56b608cb6704fb29b9c6b7bffb17a
SimpleLin <171250015@smail.nju.edu.cn>
2018-08-09 09:34:43 +0800

    +course Entity

---
+++ src/main/java/com/littlefrog/common/Tag.java
@@ -0,0 +1,6 @@
+package com.littlefrog.common;
+
+public enum Tag {
+    TAG1,
+    TAG2
+}
---
+++ src/main/java/com/littlefrog/entity/Course.java
@@ -0,0 +1,165 @@
+package com.littlefrog.entity;
+
+import com.littlefrog.common.Tag;
+
+import javax.persistence.*;
+import java.sql.Time;
+import java.sql.Timestamp;
+import java.util.Date;
+
+@Entity (name = "course")
+public class Course {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    public Course(String location, String name, String teacher, String introduction, int popularity, Tag tag, String coverPic, Date releaseTime, double price, int courseNum) {
+        this.location = location;
+        this.name = name;
+        this.teacher = teacher;
+        this.introduction = introduction;
+        this.popularity = popularity;
+        this.tag = tag;
+        this.coverPic = coverPic;
+        this.releaseTime = releaseTime;
+        this.price = price;
+        this.courseNum = courseNum;
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "id=" + id +
+                ", location='" + location + '\'' +
+                ", name='" + name + '\'' +
+                ", teacher='" + teacher + '\'' +
+                ", introduction='" + introduction + '\'' +
+                ", popularity=" + popularity +
+                ", tag=" + tag +
+                ", coverPic='" + coverPic + '\'' +
+                ", releaseTime=" + releaseTime +
+                ", price=" + price +
+                ", courseNum=" + courseNum +
+                '}';
+    }
+
+    @Column
+    private String location;
+
+    @Column
+    private String name;
+
+    @Column
+    private String teacher;
+
+    @Column
+    private String introduction;
+
+    @Column
+    private int popularity;
+
+    @Column
+    private Tag tag;
+
+    @Column
+    private String coverPic;
+
+    @Column
+    private Date releaseTime;
+
+    @Column
+    private double price;
+
+    @Column
+    private int courseNum;
+
+
+    public Integer getId() {
+        return id;
+    }
+    public String getLocation() {
+        return location;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getTeacher() {
+        return teacher;
+    }
+
+    public String getIntroduction() {
+        return introduction;
+    }
+
+    public int getPopularity() {
+        return popularity;
+    }
+
+    public Tag getTag() {
+        return tag;
+    }
+
+    public String getCoverPic() {
+        return coverPic;
+    }
+
+    public Date getReleaseTime() {
+        return releaseTime;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getCourseNum() {
+        return courseNum;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setTeacher(String teacher) {
+        this.teacher = teacher;
+    }
+
+    public void setIntroduction(String introduction) {
+        this.introduction = introduction;
+    }
+
+    public void setPopularity(int popularity) {
+        this.popularity = popularity;
+    }
+
+    public void setTag(Tag tag) {
+        this.tag = tag;
+    }
+
+    public void setCoverPic(String coverPic) {
+        this.coverPic = coverPic;
+    }
+
+    public void setReleaseTime(Date releaseTime) {
+        this.releaseTime = releaseTime;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setCourseNum(int courseNum) {
+        this.courseNum = courseNum;
+    }
+}

5aa62003b614d9e64c8a5266b3741f59672d0c1b
SimpleLin <171250015@smail.nju.edu.cn>
2018-08-26 18:56:38 +0800

    ..(仅是为了避免冲突

--- src/main/java/com/littlefrog/controller/LessonController.java
+++ src/main/java/com/littlefrog/controller/LessonController.java
@@ -20,6 +20,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.view.UrlBasedViewResolver;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
@@ -45,28 +46,4 @@ public class LessonController {
         }
     }
 
-    //跳转到上传文件的页面
-    @RequestMapping(value="/gouploadimg", method = RequestMethod.GET)
-    public String goUploadImg() {
-        //跳转到 templates 目录下的 uploadimg.html
-        return "uploadimg.html";
-    }
-
-    //处理文件上传
-    @RequestMapping(value="/testuploadimg", method = RequestMethod.POST)
-    public @ResponseBody String uploadImg(@RequestParam("file") MultipartFile file,
-                                          HttpServletRequest request) {
-        String contentType = file.getContentType();
-        String fileName = file.getOriginalFilename();
-        /*System.out.println("fileName-->" + fileName);
-        System.out.println("getContentType-->" + contentType);*/
-        String filePath = request.getSession().getServletContext().getRealPath("imgupload/");
-        try {
-            FileUtil.uploadFile(file.getBytes(), filePath, fileName);
-        } catch (Exception e) {
-            // TODO: handle exception
-        }
-        //返回json
-        return "uploadimg success";
-    }
 }

fd131053599e0f09039fdea1dba85ed0ec791c3a
151220103 <wanghui_151220103@163.com>
2018-09-02 17:52:38 +0800

    fix

--- src/main/java/com/littlefrog/service/UserService.java
+++ src/main/java/com/littlefrog/service/UserService.java
@@ -12,12 +12,15 @@ import sun.misc.BASE64Encoder;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.*;
+import java.math.BigInteger;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.security.NoSuchAlgorithmException;
 import java.util.*;
 import java.security.MessageDigest;
+
+import static com.littlefrog.common.pay.MD5Util.createMD5Sign;
 
 
 @Service("UserService")
@@ -92,30 +95,34 @@ public class UserService {
         }
         return new String(text);
     }
+    public static String getMD5(String str) {
+        try {
+            // 生成一个MD5加密计算摘要
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            // 计算md5函数
+            md.update(str.getBytes());
+            // digest()最后确定返回md5 hash值，返回值为8为字符串。因为md5 hash值是16位的hex值，实际上就是8位的字符
+            // BigInteger函数则将8位的字符串转换成16位hex值，用字符串来表示；得到字符串形式的hash值
+            String md5=new BigInteger(1, md.digest()).toString(16);
+            //BigInteger会把0省略掉，需补全至32位
+            return fillMD5(md5);
+        } catch (Exception e) {
+            throw new RuntimeException("MD5加密错误:"+e.getMessage(),e);
+        }
+    }
+
+    public static String fillMD5(String md5){
+        return md5.length()==32?md5:fillMD5("0"+md5);
+    }
     public String Md5Sign(Map params){
         String stringTemp="";
         String newstr="";
-        Queue<String> paramsSet=new PriorityQueue<String>();
-        for(Object key : params.keySet()){
-            paramsSet.add(key+"="+params.get(key));
-        }
-        for(String string : paramsSet){
-            if(stringTemp!="")
-                stringTemp+="&";
-            stringTemp+=string;
-        }
+        stringTemp ="appid="+params.get("appid")+"&body="+params.get("body")+"&mch_id="+params.get("mch_id")+"&nonce_str="+params.get("nonce_str")+"&notify_url="+params.get("notify_url")+"&out_trade_no="+params.get("out_trade_no")+"&sign_type=MD5"+"&spbill_create_ip="+params.get("spbill_create_ip")+"&total_fee="+params.get("total_fee")+"&trade_type="+params.get("trade_type");
         stringTemp+="&key="+this.key;
-        try {
-            MessageDigest md5=MessageDigest.getInstance("MD5");
-            BASE64Encoder base64en = new BASE64Encoder();
-            //加密后的字符串
-             newstr=base64en.encode(md5.digest(stringTemp.getBytes("utf-8")));
-        }catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        }
-        return newstr;
+        System.out.println(stringTemp);
+        newstr=createMD5Sign(stringTemp);
+        System.out.println(newstr.toUpperCase());
+        return newstr.toUpperCase();
     }
 
     public Object login(String code) {
